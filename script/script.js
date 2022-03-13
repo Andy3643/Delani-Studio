3573db57671b76661781d1b6376aa7c54c667407
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-03-13 11:39:37 +0300

    Style: Add hide text on click of text

--- script/script.js
+++ script/script.js
@@ -3,19 +3,33 @@ let developText = document.getElementById("develop");
 let productText = document.getElementById("product");
 
 let designIcon = document.getElementById("design-icon");
+let developIcon = document.getElementById("develop-icon");
+let productIcon = document.getElementById("product-icon");
+//To display what we do descriptions
 designIcon.addEventListener("click",()=>{
     designText.style.display = "block";
     designIcon.style.display = "none";
 })
 
-let developIcon = document.getElementById("develop-icon");
 developIcon.addEventListener("click",()=>{
     developText.style.display = "block";
     developIcon.style.display = "none";
 })
 
-let productIcon = document.getElementById("product-icon");
 productIcon.addEventListener("click",()=>{
     productText.style.display = "block";
     productIcon.style.display = "none";
+})
+// To hide text description and display icons.
+designText.addEventListener("click",()=>{
+    designIcon.style.display = "block";
+    designText.style.display = "none";
+})
+developText.addEventListener("click",()=>{
+    developIcon.style.display = "block";
+    developText.style.display = "none";
+})
+productText.addEventListener("click",()=>{
+    productIcon.style.display = "block";
+    productText.style.display = "none";
 })
